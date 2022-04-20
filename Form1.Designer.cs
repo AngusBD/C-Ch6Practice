87af4b16f23b288565b2c6d9d36005bb1262ad9f
AngusBD <97964789+AngusBD@users.noreply.github.com>
2022-04-21 01:37:42 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Ch6Practice2 {
+		partial class Form1 {
+				/// <summary>
+				/// 設計工具所需的變數。
+				/// </summary>
+				private System.ComponentModel.IContainer components = null;
+
+				/// <summary>
+				/// 清除任何使用中的資源。
+				/// </summary>
+				/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+				protected override void Dispose(bool disposing) {
+						if(disposing && (components != null)) {
+								components.Dispose();
+						}
+						base.Dispose(disposing);
+				}
+
+				#region Windows Form 設計工具產生的程式碼
+
+				/// <summary>
+				/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+				/// 這個方法的內容。
+				/// </summary>
+				private void InitializeComponent() {
+						this.groupBox1 = new System.Windows.Forms.GroupBox();
+						this.radioButton3 = new System.Windows.Forms.RadioButton();
+						this.radioButton2 = new System.Windows.Forms.RadioButton();
+						this.radioButton1 = new System.Windows.Forms.RadioButton();
+						this.groupBox2 = new System.Windows.Forms.GroupBox();
+						this.radioButton5 = new System.Windows.Forms.RadioButton();
+						this.radioButton4 = new System.Windows.Forms.RadioButton();
+						this.groupBox3 = new System.Windows.Forms.GroupBox();
+						this.checkBox2 = new System.Windows.Forms.CheckBox();
+						this.checkBox1 = new System.Windows.Forms.CheckBox();
+						this.label1 = new System.Windows.Forms.Label();
+						this.groupBox1.SuspendLayout();
+						this.groupBox2.SuspendLayout();
+						this.groupBox3.SuspendLayout();
+						this.SuspendLayout();
+						// 
+						// groupBox1
+						// 
+						this.groupBox1.Controls.Add(this.radioButton3);
+						this.groupBox1.Controls.Add(this.radioButton2);
+						this.groupBox1.Controls.Add(this.radioButton1);
+						this.groupBox1.Location = new System.Drawing.Point(37, 49);
+						this.groupBox1.Name = "groupBox1";
+						this.groupBox1.Size = new System.Drawing.Size(200, 241);
+						this.groupBox1.TabIndex = 0;
+						this.groupBox1.TabStop = false;
+						this.groupBox1.Text = "groupBox1";
+						// 
+						// radioButton3
+						// 
+						this.radioButton3.AutoSize = true;
+						this.radioButton3.Location = new System.Drawing.Point(35, 167);
+						this.radioButton3.Name = "radioButton3";
+						this.radioButton3.Size = new System.Drawing.Size(102, 19);
+						this.radioButton3.TabIndex = 2;
+						this.radioButton3.TabStop = true;
+						this.radioButton3.Text = "radioButton3";
+						this.radioButton3.UseVisualStyleBackColor = true;
+						this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+						// 
+						// radioButton2
+						// 
+						this.radioButton2.AutoSize = true;
+						this.radioButton2.Location = new System.Drawing.Point(35, 108);
+						this.radioButton2.Name = "radioButton2";
+						this.radioButton2.Size = new System.Drawing.Size(102, 19);
+						this.radioButton2.TabIndex = 1;
+						this.radioButton2.TabStop = true;
+						this.radioButton2.Text = "radioButton2";
+						this.radioButton2.UseVisualStyleBackColor = true;
+						this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+						// 
+						// radioButton1
+						// 
+						this.radioButton1.AutoSize = true;
+						this.radioButton1.Location = new System.Drawing.Point(35, 53);
+						this.radioButton1.Name = "radioButton1";
+						this.radioButton1.Size = new System.Drawing.Size(102, 19);
+						this.radioButton1.TabIndex = 0;
+						this.radioButton1.TabStop = true;
+						this.radioButton1.Text = "radioButton1";
+						this.radioButton1.UseVisualStyleBackColor = true;
+						this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+						// 
+						// groupBox2
+						// 
+						this.groupBox2.Controls.Add(this.radioButton5);
+						this.groupBox2.Controls.Add(this.radioButton4);
+						this.groupBox2.Location = new System.Drawing.Point(294, 49);
+						this.groupBox2.Name = "groupBox2";
+						this.groupBox2.Size = new System.Drawing.Size(200, 241);
+						this.groupBox2.TabIndex = 1;
+						this.groupBox2.TabStop = false;
+						this.groupBox2.Text = "groupBox2";
+						// 
+						// radioButton5
+						// 
+						this.radioButton5.AutoSize = true;
+						this.radioButton5.Location = new System.Drawing.Point(38, 167);
+						this.radioButton5.Name = "radioButton5";
+						this.radioButton5.Size = new System.Drawing.Size(102, 19);
+						this.radioButton5.TabIndex = 2;
+						this.radioButton5.TabStop = true;
+						this.radioButton5.Text = "radioButton5";
+						this.radioButton5.UseVisualStyleBackColor = true;
+						this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+						// 
+						// radioButton4
+						// 
+						this.radioButton4.AutoSize = true;
+						this.radioButton4.Location = new System.Drawing.Point(38, 53);
+						this.radioButton4.Name = "radioButton4";
+						this.radioButton4.Size = new System.Drawing.Size(102, 19);
+						this.radioButton4.TabIndex = 1;
+						this.radioButton4.TabStop = true;
+						this.radioButton4.Text = "radioButton4";
+						this.radioButton4.UseVisualStyleBackColor = true;
+						this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+						// 
+						// groupBox3
+						// 
+						this.groupBox3.Controls.Add(this.checkBox2);
+						this.groupBox3.Controls.Add(this.checkBox1);
+						this.groupBox3.Location = new System.Drawing.Point(552, 49);
+						this.groupBox3.Name = "groupBox3";
+						this.groupBox3.Size = new System.Drawing.Size(200, 241);
+						this.groupBox3.TabIndex = 1;
+						this.groupBox3.TabStop = false;
+						this.groupBox3.Text = "groupBox3";
+						// 
+						// checkBox2
+						// 
+						this.checkBox2.AutoSize = true;
+						this.checkBox2.Location = new System.Drawing.Point(31, 168);
+						this.checkBox2.Name = "checkBox2";
+						this.checkBox2.Size = new System.Drawing.Size(91, 19);
+						this.checkBox2.TabIndex = 1;
+						this.checkBox2.Text = "checkBox2";
+						this.checkBox2.UseVisualStyleBackColor = true;
+						this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+						// 
+						// checkBox1
+						// 
+						this.checkBox1.AutoSize = true;
+						this.checkBox1.Location = new System.Drawing.Point(31, 53);
+						this.checkBox1.Name = "checkBox1";
+						this.checkBox1.Size = new System.Drawing.Size(91, 19);
+						this.checkBox1.TabIndex = 0;
+						this.checkBox1.Text = "checkBox1";
+						this.checkBox1.UseVisualStyleBackColor = true;
+						this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+						// 
+						// label1
+						// 
+						this.label1.AutoSize = true;
+						this.label1.Font = new System.Drawing.Font("新細明體", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+						this.label1.Location = new System.Drawing.Point(37, 359);
+						this.label1.Name = "label1";
+						this.label1.Size = new System.Drawing.Size(101, 37);
+						this.label1.TabIndex = 2;
+						this.label1.Text = "label1";
+						// 
+						// Form1
+						// 
+						this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+						this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+						this.ClientSize = new System.Drawing.Size(800, 450);
+						this.Controls.Add(this.label1);
+						this.Controls.Add(this.groupBox2);
+						this.Controls.Add(this.groupBox3);
+						this.Controls.Add(this.groupBox1);
+						this.Name = "Form1";
+						this.Text = "Form1";
+						this.Load += new System.EventHandler(this.Form1_Load);
+						this.groupBox1.ResumeLayout(false);
+						this.groupBox1.PerformLayout();
+						this.groupBox2.ResumeLayout(false);
+						this.groupBox2.PerformLayout();
+						this.groupBox3.ResumeLayout(false);
+						this.groupBox3.PerformLayout();
+						this.ResumeLayout(false);
+						this.PerformLayout();
+
+				}
+
+				#endregion
+
+				private System.Windows.Forms.GroupBox groupBox1;
+				private System.Windows.Forms.RadioButton radioButton3;
+				private System.Windows.Forms.RadioButton radioButton2;
+				private System.Windows.Forms.RadioButton radioButton1;
+				private System.Windows.Forms.GroupBox groupBox2;
+				private System.Windows.Forms.RadioButton radioButton5;
+				private System.Windows.Forms.RadioButton radioButton4;
+				private System.Windows.Forms.GroupBox groupBox3;
+				private System.Windows.Forms.CheckBox checkBox2;
+				private System.Windows.Forms.CheckBox checkBox1;
+				private System.Windows.Forms.Label label1;
+		}
+}
+
